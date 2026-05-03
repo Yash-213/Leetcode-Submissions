12bb18159198424ae721f7521f06416c19e17b20
Yash Waychal <yash.waychal02@gmail.com>
2026-05-03 17:31:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -3,11 +3,9 @@ class Solution {
         Arrays.sort(piles);
         int sum = 0;
         int a = piles.length / 3;
-        for(int i = piles.length - 2; i > 0; i -= 2) {
-            if (a > 0){
-                sum += piles[i];
-                a--;
-            }else break;
+        for (int i = piles.length - 2; a > 0; i -= 2) {
+            sum += piles[i];
+            a--;
         }
         return sum;
     }
