6ec461f29776fc0c33c4128e261e6a99f41c54a8
Yash Waychal <yash.waychal02@gmail.com>
2026-05-21 08:03:06 +0530

    Time: 63 ms (61.35%), Space: 81.9 MB (48.38%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -1,44 +1,22 @@
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
         HashSet<Integer> set = new HashSet<>();
-        for(int i = 0; i < arr1.length; i++) {
-            while(arr1[i] > 0) {
-                set.add(arr1[i]);
-                arr1[i] /= 10;
+        for(int i : arr1) {
+            while(i > 0) {
+                set.add(i);
+                i /= 10;
             }
         }
         int max = 0;
-        for(int i = 0; i < arr2.length; i++) {
-            while(arr2[i] > 0) {
-                if (set.contains(arr2[i])) {
-                    max = Math.max(max, String.valueOf(arr2[i]).length());
+        for(int i : arr2) {
+            while(i > 0) {
+                if (set.contains(i)) {
+                    max = Math.max(max, (int)Math.log10(i) + 1);
                     break;
                 }
-                arr2[i] /= 10;
+                i /= 10;
             }
         }
         return max;
     }
 }
-// public int longestCommonPrefix(int[] arr1, int[] arr2) {
-    //     int max = Integer.MIN_VALUE;
-    //     for(int i = 0; i < arr1.length; i++) {
-    //         for(int j = 0; j < arr2.length; j++) {
-    //             int a = commonPrefix(arr1[i], arr2[j]);
-    //             max = Math.max(a, max);
-    //         }
-    //     }
-    //     return max;
-    // }
-    // static int commonPrefix(int a, int b) {
-    //     int count = 0;
-    //     String as = Integer.toString(a);
-    //     String bs = Integer.toString(b);
-    //     int i = 0;
-    //     while(as.length() > i && bs.length() > i) {
-    //         if (as.charAt(i) == bs.charAt(i)) count++;
-    //         else break;
-    //         i++;
-    //     }
-    //     return count;
-    // }
