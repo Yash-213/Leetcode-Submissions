3f5ca405a8630d87975e522c8bf98e0119eeb269
Yash Waychal <yash.waychal02@gmail.com>
2026-06-16 15:24:44 +0530

    Time: 3 ms (100%), Space: 55.3 MB (48.53%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '#') {
+                str.append(str);
+            } else if (s.charAt(i) == '*') {
+                if (str.length() != 0) {
+                    str = str.deleteCharAt(str.length() - 1);
+                }
+            } 
+            else if (s.charAt(i) == '%') str.reverse();
+            else str = str.append(s.charAt(i));
+        }
+        return str.toString();
+    }
+}
