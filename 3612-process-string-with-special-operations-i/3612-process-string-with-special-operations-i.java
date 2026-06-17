20182209991b256641b95a33ed03081ba81ca5ea
Yash Waychal <yash.waychal02@gmail.com>
2026-06-17 13:52:10 +0530

    Time: 3 ms (100%), Space: 55.2 MB (60%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -2,15 +2,15 @@ class Solution {
     public String processStr(String s) {
         StringBuilder str = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '#') {
+            if (s.charAt(i) == '#')
                 str.append(str);
-            } else if (s.charAt(i) == '*') {
-                if (str.length() != 0) {
-                    str = str.deleteCharAt(str.length() - 1);
-                }
-            } 
-            else if (s.charAt(i) == '%') str.reverse();
-            else str = str.append(s.charAt(i));
+            else if (s.charAt(i) == '*') {
+                if (str.length() != 0)
+                    str.deleteCharAt(str.length() - 1);
+            } else if (s.charAt(i) == '%')
+                str.reverse();
+            else
+                str.append(s.charAt(i));
         }
         return str.toString();
     }
