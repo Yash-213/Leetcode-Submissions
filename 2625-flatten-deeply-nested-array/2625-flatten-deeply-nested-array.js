672086b6c4ffc571cbfc984763fd61d9574d90a3
Yash Waychal <yash.waychal02@gmail.com>
2026-07-02 08:52:30 +0530

    Time: 73 ms (84.13%), Space: 71.5 MB (95.95%) - LeetHub

---
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n, tempArray = [], currentCycle = 0) {
+    for (let index = 0; index < arr.length; index++) {
+        if (Array.isArray(arr[index]) && currentCycle < n) 
+            flat(arr[index], n, tempArray, currentCycle + 1)
+        else tempArray.push(arr[index])
+    }
+    return tempArray
+};
