df533ed753305599645725cfa8adcf4abd40dfb1
Yash Waychal <yash.waychal02@gmail.com>
2026-07-02 14:27:53 +0530

    Time: 115 ms (5.51%), Space: 49.9 MB (78.96%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,14 +1,20 @@
 class Solution {
-    static public List<Integer> lexicalOrder(int n) {
-        List<Integer> list = new ArrayList<>();
-        
-        for (int i = 1; i <= n; i++) list.add(i);
+    public List<Integer> lexicalOrder(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
 
-        list.sort((a, b) -> {
-            String s1 = String.valueOf(a);
-            String s2 = String.valueOf(b);
-            return s1.compareTo(s2);
-        });
+        for (int i = 1; i <= 9; i++)
+            lexical(list, i, n);
+
         return list;
+    }
+
+    private static void lexical(ArrayList<Integer> list, int i, int n) {
+        if (i > n) return;
+        
+        list.add(i);
+        i *= 10;
+
+        for (int j = 0; j <= 9; j++)
+            lexical(list, i + j, n);
     }
 }
