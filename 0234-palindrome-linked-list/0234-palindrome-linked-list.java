5750cb9b94d35c851f8530f0b6c93f04e00a8930
Yash Waychal <yash.waychal02@gmail.com>
2026-07-07 11:49:20 +0530

    Time: 11 ms (13.9%), Space: 101.5 MB (10.87%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,48 +10,21 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode mid = middleNode(head);
-        ListNode headSecond = reverseList(mid);
-        ListNode rereverseHead = headSecond;
-
-        // compare both the halves
-        while (head != null && headSecond != null) {
-            if (head.val != headSecond.val) {
-                break;
-            }
-            head = head.next;
-            headSecond = headSecond.next;
+        List<Integer> l = new ArrayList<>();
+        ListNode h1 = head;
+        while (h1 != null) {
+            l.add(h1.val);
+            h1 = h1.next;
         }
-        reverseList(rereverseHead);
-
-        return head == null || headSecond == null;
+        return isPal(l);
     }
-    public ListNode reverseList(ListNode head) {
-        if (head == null) {
-            return head;
+    private static boolean isPal(List<Integer> l) {
+        int s = 0, e = l.size() - 1;
+        while (s < e) {
+            if (l.get(s) != l.get(e)) return false;
+            s++;
+            e--;
         }
-        ListNode prev = null;
-        ListNode present = head;
-        ListNode next = present.next;
-
-        while (present != null) {
-            present.next = prev;
-            prev = present;
-            present = next;
-            if (next != null) {
-                next = next.next;
-            }
-        }
-        return prev;
-    }
-    public ListNode middleNode(ListNode head) {
-        ListNode s = head;
-        ListNode f = head;
-
-        while (f != null && f.next != null) {
-            s = s.next;
-            f = f.next.next;
-        }
-        return s;
+        return true;
     }
 }
