46608e18a9412fdf3fff058932e17f308b5e54dc
Yash Waychal <yash.waychal02@gmail.com>
2026-07-11 13:11:44 +0530

    Time: 1 ms (79.6%), Space: 43.9 MB (55.56%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.java
@@ -1,21 +1,25 @@
 class Solution {
     public List<String> findAndReplacePattern(String[] words, String pattern) {
+        
         List<String> list = new ArrayList<>();
+        int n = pattern.length();
 
         for (int i = 0; i < words.length; i++) {
+
             char[] freq = new char[26];
             HashSet<Character> set = new HashSet<>();
-
-            if (words[i].length() != pattern.length())
+            
+            String str = words[i];
+            if (str.length() != n)
                 continue;
 
             boolean flag = true;
-            for (int j = 0; j < pattern.length(); j++) {
-                
-                int a = pattern.charAt(j) - 'a';
-                char c = words[i].charAt(j);
+            for (int j = 0; j < n; j++) {
 
-                if (freq[a] == 0 && !set.contains(words[i].charAt(j))) {
+                int a = pattern.charAt(j) - 'a';
+                char c = str.charAt(j);
+
+                if (freq[a] == 0 && !set.contains(c)) {
                     freq[a] = c;
                     set.add(c);
                 } else if (freq[a] != c) {
@@ -24,7 +28,7 @@ class Solution {
                 }
             }
 
-            if (flag) list.add(words[i]);
+            if (flag) list.add(str);
         }
         return list;
     }
