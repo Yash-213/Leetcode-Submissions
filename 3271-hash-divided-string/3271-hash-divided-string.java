a0eda19055dbe7660f9aa634ae471448f7814d16
Yash Waychal <yash.waychal02@gmail.com>
2026-08-10 22:16:55 +0530

    Time: 2 ms (98.08%), Space: 46.3 MB (90.63%) - LeetHub

---
+++ 3271-hash-divided-string/3271-hash-divided-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String stringHash(String s, int k) {
+        char[] ch = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ch.length; i += k) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) sum += ch[j] - 'a';
+            sb.append((char)('a' + (sum % 26)));
+        }
+        return sb.toString();
+    }
+}
