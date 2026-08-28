6dccf6b1ceec05a1145611b56d3fc1b31fbaa460
Yash Waychal <yash.waychal02@gmail.com>
2026-08-28 11:36:19 +0530

    Time: 0 ms (100%), Space: 44 MB (36.09%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.java
@@ -1,21 +1,21 @@
 class Solution {
     public int countValidSelections(int[] nums) {
-        int n = nums.length;
-        int[] prefix = new int[n];
-        prefix[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] + nums[i];
-        }
-        int[] sufix = new int[n];
-        sufix[n - 1] = nums[n - 1];
-        for (int i = n - 2; i > -1; i--) {
-            sufix[i] = sufix[i + 1] + nums[i];
-        }
+        int sum = 0;
+        for (int n : nums) sum += n;
+
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0 && prefix[i] == sufix[i]) count += 2;
-            else if (nums[i] == 0 && Math.abs(prefix[i] - sufix[i]) == 1) count++;
+        int rem = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                int diff = sum - rem;
+                if (diff == rem)
+                    count += 2;
+                else if (Math.abs(diff - rem) == 1)
+                    count++;
+            }
+            rem += nums[i];
         }
+        
         return count;
     }
 }
