a1aab328846d86d0ffac91d6f472e7167fd505ee
Yash Waychal <yash.waychal02@gmail.com>
2026-09-26 07:34:16 +0530

    Time: 50 ms (60%), Space: 93.4 MB (96.67%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.js
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.js
@@ -3,25 +3,25 @@
  * @param {string[][]} knowledge
  * @return {string}
  */
-var evaluate = function(s, knowledge) {
+var evaluate = function (s, knowledge) {
     const map = new Map();
-        for (const list of knowledge)
-            map.set(list[0], list[1]);
+    for (const list of knowledge)
+        map.set(list[0], list[1]);
 
-        let sb = "";
-        let l = 0;
-        while (l < s.length) {
-            if (s[l] != '(') 
-                sb += s[l++];
-            else {
-                let r = l + 1;
-                while (s[r] != ')') r++;
+    let sb = "";
+    let l = 0;
+    while (l < s.length) {
+        if (s[l] != '(')
+            sb += s[l++];
+        else {
+            let r = l + 1;
+            while (s[r] != ')') r++;
 
-                const sub = s.substring(l + 1, r);
-                sb += map.get(sub) ?? "?";
-                
-                l = r + 1;
-            }
+            const sub = s.substring(l + 1, r);
+            sb += map.get(sub) ?? "?";
+
+            l = r + 1;
         }
-        return sb;
+    }
+    return sb;
 };
